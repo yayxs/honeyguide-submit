2c6ec3d743c907afdd643f7235fe270829454f9e
yayxs <leaderywl@163.com>
2024-08-23 18:03:08 +0800

    chore: honox

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import pages from '@hono/vite-cloudflare-pages'
+import adapter from '@hono/vite-dev-server/cloudflare'
+import honox from 'honox/vite'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [honox({})]
 })
